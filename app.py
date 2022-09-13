02473d32b4a18a506b93e6d6a226fecef98412f3
Nancy Pareta <Nancy_Pareta@mckinsey.com>
2022-09-13 10:04:45 +0530

    commit app

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h2>This is my first app</h2>'
+    return '<h2>This is my first app by Nancy </h2>'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
